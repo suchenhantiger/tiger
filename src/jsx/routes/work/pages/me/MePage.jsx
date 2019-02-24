491cb34db658ea722670843656695d3873f0234d
luorui41 <405683394@qq.com>
2019-02-24 21:52:45 +0800

    优化

--- src/jsx/routes/work/pages/me/MePage.jsx
+++ src/jsx/routes/work/pages/me/MePage.jsx
@@ -12,19 +12,46 @@ class MePage extends PageComponent {
     //获取页面名称
     getPageName() { return "我的主页"; }
 
-    settingClick = ()=>{
+    settingClick = () => {
 
     }
 
-    msgClick = ()=>{
+    msgClick = () => {
 
     }
 
-    renderIcons(){
+    renderIcons() {
         return [
-            <HeaderIcon iconCls={styles.setting} onClick={this.settingClick}/>,
-            <HeaderIcon iconCls={styles.msg} onClick={this.msgClick}/>
+            <HeaderIcon iconCls={styles.setting} onClick={this.settingClick} />,
+            <HeaderIcon iconCls={styles.msg} onClick={this.msgClick} />
         ]
+    }
+
+    renderItem(text, value) {
+        return (
+            <li>
+                <p className="font32">{value}</p>
+                <p className={this.mergeClassName("c9", "mg-tp-10")}>{text}</p>
+            </li>
+        )
+    }
+
+    renderFuncItem(text, icon, onClick) {
+        return (
+            <li onClick={onClick}>
+                <p><img src={icon} /></p>
+                <p>{text}</p>
+            </li>
+        )
+    }
+
+    renderListItem(text, icon, isRed, onClick) {
+        return (
+            <li onClick={onClick}>
+                <div className={styles.list_icon}><img src={icon} /> </div>
+                <div className={styles.list_text}>{text}{isRed ? <i className={"red"}></i> : null}</div>
+            </li>
+        )
     }
 
     render() {
@@ -53,69 +80,30 @@ class MePage extends PageComponent {
                             <div className={"clear"}></div>
                             <div className={styles.account_dt}>
                                 <ul>
-                                    <li>
-                                        <p className="font32">$1888.00</p>
-                                        <p className={this.mergeClassName("c9", "mg-tp-10")}>浮动盈亏</p>
-                                    </li>
-                                    <li>
-                                        <p className="font32">$688.00</p>
-                                        <p className={this.mergeClassName("c9", "mg-tp-10")}>账户净值</p>
-                                    </li>
-                                    <li>
-                                        <p className="font32">1.00%</p>
-                                        <p className={this.mergeClassName("c9", "mg-tp-10")}>保证金比例</p>
-                                    </li>
+                                    {this.renderItem("浮动盈亏", "$1888.00")}
+                                    {this.renderItem("账户净值", "$688.00")}
+                                    {this.renderItem("保证金比例", "1.00%")}
                                 </ul>
                             </div>
                         </div>
                     </div>
                     <div className={this.mergeClassName(styles.optional_detail, styles.mt3)}>
                         <ul className={styles.account_icons}>
-                            <li>
-                                <p><img src="./images/me/icon-recharge.png" /></p>
-                                <p>充值</p>
-                            </li>
-                            <li>
-                                <p><img src="./images/me/icon-recharge.png" /></p>
-                                <p>提现</p>
-                            </li>
-                            <li>
-                                <p><img src="./images/me/icon-recharge.png" /></p>
-                                <p>银行卡</p>
-                            </li>
-                            <li>
-                                <p><img src="./images/me/icon-recharge.png" /></p>
-                                <p>钱包</p>
-                            </li>
+                            {this.renderFuncItem("充值", "./images/me/icon-recharge.png")}
+                            {this.renderFuncItem("提现", "./images/me/icon-recharge.png")}
+                            {this.renderFuncItem("银行卡", "./images/me/icon-recharge.png")}
+                            {this.renderFuncItem("钱包", "./images/me/icon-recharge.png")}
                         </ul>
                     </div>
                     <div className={this.mergeClassName(styles.ht_gray, styles.mt3)}></div>
                     <div className={styles.icon_list}>
                         <ul>
-                            <li>
-                                <div className={styles.list_icon}><img src="./images/me/icon-list01.png" /> </div>
-                                <div className={styles.list_text}>我的钱包<i className={"red"}></i></div>
-                            </li>
-                            <li>
-                                <div className={styles.list_icon}><img src="./images/me/icon-list02.png" /> </div>
-                                <div className={styles.list_text}>邀请好友</div>
-                            </li>
-                            <li>
-                                <div className={styles.list_icon}><img src="./images/me/icon-list03.png" /> </div>
-                                <div className={styles.list_text}>每日汇评</div>
-                            </li>
-                            <li>
-                                <div className={styles.list_icon}><img src="./images/me/icon-list04.png" /> </div>
-                                <div className={styles.list_text}>财经日历</div>
-                            </li>
-                            <li>
-                                <div className={styles.list_icon}><img src="./images/me/icon-list05.png" /> </div>
-                                <div className={styles.list_text}>帮助中心</div>
-                            </li>
-                            <li>
-                                <div className={styles.list_icon}><img src="./images/me/icon-list06.png" /> </div>
-                                <div className={styles.list_text}>联系客服</div>
-                            </li>
+                            {this.renderListItem("我的钱包", "./images/me/icon-list01.png", true)}
+                            {this.renderListItem("邀请好友", "./images/me/icon-list02.png", false)}
+                            {this.renderListItem("每日汇评", "./images/me/icon-list03.png", false)}
+                            {this.renderListItem("财经日历", "./images/me/icon-list04.png", false)}
+                            {this.renderListItem("帮助中心", "./images/me/icon-list05.png", false)}
+                            {this.renderListItem("联系客服", "./images/me/icon-list06.png", false)}
                         </ul>
                     </div>
                 </Content>
