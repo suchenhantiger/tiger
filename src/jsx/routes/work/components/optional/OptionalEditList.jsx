fd91c7f1b82ce4c8771317bfc01800b1e5a1c404
luorui41 <405683394@qq.com>
2019-03-09 20:18:29 +0800

    排序控件

--- src/jsx/routes/work/components/optional/OptionalEditList.jsx
+++ src/jsx/routes/work/components/optional/OptionalEditList.jsx
@@ -1,28 +1,64 @@
 import { connect } from 'react-redux';
-import {getOptionalList} from "../../actions/optional/optionalAction";
+import { getOptionalList } from "../../actions/optional/optionalAction";
+
+import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc';
 
 import styles from './css/optionalEditList.less';
 
-class OptionalEditList extends PureComponent{
+class OptionalEditList extends PureComponent {
 
     //构造函数
     constructor(props) {
         super(props);
         this.state = {
-            list:[1,1,11,1,1,1,1,1,1,1,1,]
+            list: [1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1,]
         };
+        this.SortableItem = this.getItemElement();
+        this.SortableList = this.getListElement();
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.getOptionalList(this, {}, this.updateList);
     }
 
-    updateList = (list)=>{
-        this.setState({list})
+    updateList = (list) => {
+        this.setState({ list })
     }
 
-    renderList(list){
-        return list.map(item=>{
+    getListElement(){
+        var {SortableItem} = this;
+        return SortableContainer(({ items }) => {
+            return (
+                <ul className={styles.optional_list}>
+                    {items.map((value, index) => (
+                        <SortableItem key={`item-${index}`} index={index} data={value} />
+                    ))}
+                </ul>
+            );
+        });
+    }
+
+    getItemElement(){
+        return SortableElement(({ data }) => {
+            return (
+                <li className={styles.op_sort_li}>
+                    <div className={styles.list_left_icon}>
+                        <div className={styles.icon_delete_red}></div>
+                    </div>
+                    <div className={styles.currency_name}>
+                        <p className={this.mergeClassName("c3", styles.c3)}>欧元美元</p>
+                        <p className={this.mergeClassName("c9", "font-arial")}>EURUSD200</p>
+                    </div>
+                    <div className={styles.list_right_icon}>
+                        <div className={styles.icon_sort}></div>
+                    </div>
+                </li>
+            )
+        });
+    }
+
+    renderList(list) {
+        return list.map(item => {
             return (
                 <li className={styles.op_sort_li}>
                     <div className={styles.list_left_icon}>
@@ -40,22 +76,28 @@ class OptionalEditList extends PureComponent{
         })
     }
 
-    //渲染函数
-    render(){
+    onSortEnd = ({ oldIndex, newIndex }) => {
+        // this.setState(({ items }) => ({
+        //     items: arrayMove(items, oldIndex, newIndex),
+        // }));
+        console.log(oldIndex, newIndex);
+    };
 
-        var {list} = this.state;
+    //渲染函数
+    render() {
+
+        var { list } = this.state,
+            {SortableList} = this;
 
         return (
-            <ul className={styles.optional_list}>
-                {this.renderList(list)}
-            </ul>
+            <SortableList items={[1, 2, 3, 4, 5]} onSortEnd={this.onSortEnd} />
         );
     }
 
 }
 
-function injectAction(){
-    return {getOptionalList};
+function injectAction() {
+    return { getOptionalList };
 }
 
-module.exports = connect(null,injectAction())(OptionalEditList);
+module.exports = connect(null, injectAction())(OptionalEditList);
